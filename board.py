8532e4633ff143ebc92dcb63d0ea8b72ff272099
Kainoa Gardner <kainoagardner123@gmail.com>
2024-01-28 13:21:54 -0800

    first

--- board.py
+++ board.py
@@ -13,6 +13,8 @@ class Board():
                         [0, 0, 0, 0, 0, 0, 0, 0]]
         self.blackTurn = True
         self.mosPos = pygame.mouse.get_pos()
+        self.flipPiece = []
+        self.count = 0
 
     def displayBoard(self):
         for r in range(len(self.board)):
@@ -56,22 +58,229 @@ class Board():
         self.mosPos = pygame.mouse.get_pos()
         if MARGINSIZE < self.mosPos[0] < WIDTH - MARGINSIZE and MARGINSIZE < self.mosPos[1] < HEIGHT - MARGINSIZE:
             if self.board[(self.mosPos[1] - MARGINSIZE) // TILESIZE][(self.mosPos[0] - MARGINSIZE) // TILESIZE] == 0:
-                if self.blackTurn:
+                if self.blackTurn and self.checkValidMove((((self.mosPos[0] - MARGINSIZE) // TILESIZE),(self.mosPos[1] - MARGINSIZE) // TILESIZE),1):
                     pygame.draw.circle(screen, "Black", (TILESIZE * ((self.mosPos[0] - MARGINSIZE) // TILESIZE) + TILESIZE //2 + MARGINSIZE,TILESIZE *((self.mosPos[1] - MARGINSIZE) // TILESIZE) + TILESIZE //2 + MARGINSIZE),TILESIZE // 2 - 3)
                     screen.blit(toumei,(TILESIZE * ((self.mosPos[0] - MARGINSIZE) // TILESIZE) + MARGINSIZE,TILESIZE *((self.mosPos[1] - MARGINSIZE) // TILESIZE)  + MARGINSIZE))
-                elif not self.blackTurn:
+                elif not self.blackTurn and self.checkValidMove((((self.mosPos[0] - MARGINSIZE) // TILESIZE),(self.mosPos[1] - MARGINSIZE) // TILESIZE),2):
                     pygame.draw.circle(screen, "White", (TILESIZE * ((self.mosPos[0] - MARGINSIZE) // TILESIZE) + TILESIZE // 2 + MARGINSIZE,TILESIZE * ((self.mosPos[1] - MARGINSIZE) // TILESIZE) + TILESIZE // 2 + MARGINSIZE),TILESIZE // 2 - 3)
                     screen.blit(toumei, (TILESIZE * ((self.mosPos[0] - MARGINSIZE) // TILESIZE) + MARGINSIZE,TILESIZE * ((self.mosPos[1] - MARGINSIZE) // TILESIZE) + MARGINSIZE))
 
     def playPiece(self,pos):
         if self.board[pos[1]][pos[0]] == 0:
-            if self.blackTurn:
+            if self.blackTurn and self.checkValidMove((((self.mosPos[0] - MARGINSIZE) // TILESIZE),(self.mosPos[1] - MARGINSIZE) // TILESIZE),1):
                 self.board[pos[1]][pos[0]] = 1
+                self.flipPieces(pos,1)
                 self.blackTurn = not self.blackTurn
-            elif not self.blackTurn:
+            elif not self.blackTurn and self.checkValidMove((((self.mosPos[0] - MARGINSIZE) // TILESIZE),(self.mosPos[1] - MARGINSIZE) // TILESIZE),2):
                 self.board[pos[1]][pos[0]] = 2
+                self.flipPieces(pos, 2)
                 self.blackTurn = not self.blackTurn
 
+    def flipPieces(self,pos,turn):
+
+        if turn == 1:
+            opponent = 2
+        else:
+            opponent = 1
+
+        self.flipPiece.clear()
+
+        for i in range(1,(pos[1] + 1)):     #DOWN
+            if self.board[pos[1] - i][pos[0]] == 0:
+                break
+            elif self.board[pos[1] - i][pos[0]] == opponent:
+                self.flipPiece.append((pos[1] - i,pos[0]))
+            elif self.board[pos[1] - i][pos[0]] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                break
+        self.flipPiece.clear()
+
+        for i in range(1,(8 - pos[1])):      #UP
+            if self.board[pos[1] + i][pos[0]] == 0:
+                break
+            elif self.board[pos[1] + i][pos[0]] == opponent:
+                self.flipPiece.append((pos[1] + i,pos[0]))
+            elif self.board[pos[1] + i][pos[0]] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                break
+        self.flipPiece.clear()
+
+        for i in range(1,(pos[0] + 1)):     #LEFT
+            if self.board[pos[1]][pos[0] - i] == 0:
+                break
+            elif self.board[pos[1]][pos[0] - i] == opponent:
+                self.flipPiece.append((pos[1],pos[0] - i))
+            elif self.board[pos[1]][pos[0] - i] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                break
+        self.flipPiece.clear()
+
+        for i in range(1,(8 - pos[0])):   #RIGHT
+            if self.board[pos[1]][pos[0] + i] == 0:
+
+                break
+            elif self.board[pos[1]][pos[0] + i] == opponent:
+                self.flipPiece.append((pos[1],pos[0] + i))
+            elif self.board[pos[1]][pos[0] + i] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                break
+        self.flipPiece.clear()
+
+        for i in range(1,min((8 - pos[1]),(8 - pos[0]))):   #UPRIGHT
+            if self.board[pos[1] + i][pos[0] + i] == 0:
+                break
+            elif self.board[pos[1] + i][pos[0] + i] == opponent:
+                self.flipPiece.append((pos[1] + i,pos[0] + i))
+            elif self.board[pos[1] + i][pos[0] + i] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                self.flipPiece.clear()
+                break
+        self.flipPiece.clear()
+
+        for i in range(1,min((8 - pos[1]),(pos[0] + 1))):   #UPLEFT
+            if self.board[pos[1] + i][pos[0] - i] == 0:
+                break
+            elif self.board[pos[1] + i][pos[0] - i] == opponent:
+                self.flipPiece.append((pos[1] + i,pos[0] - i))
+            elif self.board[pos[1] + i][pos[0] - i] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                break
+        self.flipPiece.clear()
+
+        for i in range(1,min((pos[1] + 1),(pos[0] + 1))):   #DOWNRIGHT
+            if self.board[pos[1] - i][pos[0] - i] == 0:
+                break
+            elif self.board[pos[1] - i][pos[0] - i] == opponent:
+                self.flipPiece.append((pos[1] - i,pos[0] - i))
+            elif self.board[pos[1] - i][pos[0] - i] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                break
+        self.flipPiece.clear()
+
+        for i in range(1,min((pos[1] + 1),(8 - pos[0]))):   #DOWNLEFT
+            if self.board[pos[1] - i][pos[0] + i] == 0:
+                break
+            elif self.board[pos[1] - i][pos[0] + i] == opponent:
+                self.flipPiece.append((pos[1] - i,pos[0] + i))
+            elif self.board[pos[1] - i][pos[0] + i] == turn:
+                for piece in self.flipPiece:
+                    self.board[piece[0]][piece[1]] = turn
+                break
+        self.flipPiece.clear()
+
+    def checkValidMove(self,pos,turn):
+        if turn == 1:
+            opponent = 2
+        else:
+            opponent = 1
+
+        self.count = 0
+
+        for i in range(1, (pos[1] + 1)):  # DOWN
+            if self.board[pos[1] - i][pos[0]] == 0:
+                break
+            elif self.board[pos[1] - i][pos[0]] == opponent:
+                self.count += 1
+            elif self.board[pos[1] - i][pos[0]] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+        for i in range(1, (8 - pos[1])):  # UP
+            if self.board[pos[1] + i][pos[0]] == 0:
+                break
+            elif self.board[pos[1] + i][pos[0]] == opponent:
+                self.count += 1
+            elif self.board[pos[1] + i][pos[0]] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+        for i in range(1, (pos[0] + 1)):  # LEFT
+            if self.board[pos[1]][pos[0] - i] == 0:
+                break
+            elif self.board[pos[1]][pos[0] - i] == opponent:
+                self.count += 1
+            elif self.board[pos[1]][pos[0] - i] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+
+        for i in range(1, (8 - pos[0])):  # RIGHT
+            if self.board[pos[1]][pos[0] + i] == 0:
+                break
+            elif self.board[pos[1]][pos[0] + i] == opponent:
+                self.count += 1
+            elif self.board[pos[1]][pos[0] + i] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+        for i in range(1, min((8 - pos[1]), (8 - pos[0]))):  # UPRIGHT
+            if self.board[pos[1] + i][pos[0] + i] == 0:
+                break
+            elif self.board[pos[1] + i][pos[0] + i] == opponent:
+                self.count += 1
+            elif self.board[pos[1] + i][pos[0] + i] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+        for i in range(1, min((8 - pos[1]), (pos[0] + 1))):  # UPLEFT
+            if self.board[pos[1] + i][pos[0] - i] == 0:
+                break
+            elif self.board[pos[1] + i][pos[0] - i] == opponent:
+                self.count += 1
+            elif self.board[pos[1] + i][pos[0] - i] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+        for i in range(1, min((pos[1] + 1), (pos[0] + 1))):  # DOWNRIGHT
+            if self.board[pos[1] - i][pos[0] - i] == 0:
+                break
+            elif self.board[pos[1] - i][pos[0] - i] == opponent:
+                self.count += 1
+            elif self.board[pos[1] - i][pos[0] - i] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+        for i in range(1, min((pos[1] + 1), (8 - pos[0]))):  # DOWNLEFT
+            if self.board[pos[1] - i][pos[0] + i] == 0:
+                break
+            elif self.board[pos[1] - i][pos[0] + i] == opponent:
+                self.count += 1
+            elif self.board[pos[1] - i][pos[0] + i] == turn:
+                if self.count > 0:
+                    return True
+                elif self.count == 0:
+                    break
+        self.count = 0
+
+        return False
 
     def update(self):
         self.displayTurn()
